6db819671081075adce155e3f9961c5a086ab654
Poncho0794 <benjamin.banda.reyna@hotmail.com>
2015-09-13 23:00:44 -0500

    Revert "arreglos del kenneth"
    This reverts commit 0f35cc775d6ac1950f4ec08ae43fd482cf6bf3df.

--- js/pon.js
+++ js/pon.js
@@ -8,8 +8,8 @@
 
     //create table and insert some record
     function populateDB(tx) {
-      tx.executeSql('Create Table IF NOT EXISTS categorias_ingreso(id_categoria_ingreso integer primary key, nombre_categoria_ingreso text UNIQUE)');
-      tx.executeSql('Create Table IF NOT EXISTS categorias_egreso(id_categoria_egreso integer primary key, nombre_categoria_egreso text  UNIQUE)');
+        tx.executeSql('Create Table IF NOT EXISTS categorias_ingreso(id_categoria_ingreso integer primary key, nombre_categoria_ingreso text)');
+        tx.executeSql('Create Table IF NOT EXISTS categorias_egreso(id_categoria_egreso integer primary key, nombre_categoria_egreso text)');
         tx.executeSql('Create Table IF NOT EXISTS saldos_ingreso(id_saldo_ingreso integer primary key, fecha_ingreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_ingresado real, id_categoria_ingreso integer)');
         tx.executeSql('Create Table IF NOT EXISTS saldos_egreso(id_saldo_egreso integer primary key, fecha_egreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_egresado real, id_categoria_egreso integer)');
         tx.executeSql('Create Table IF NOT EXISTS cta(id_cuenta_in integer primary key, nombre text, saldo real)');
@@ -40,6 +40,7 @@
             document.getElementById('saldo_m').innerHTML = "$ " + (ingreso - egreso);
             selectAca();
             selectPat();
+            nivel_riqueza();
           });
         });
       });
@@ -50,13 +51,10 @@
       var hoy = new Date();
       var yy = String(hoy.getFullYear());
       db.transaction(function(tx) {
-        console.log("***************************************************");
         tx.executeSql("SELECT SUM(monto_ingresado) AS ingreso from saldos_ingreso WHERE strftime('%Y', fecha_ingreso) = ? ", [yy], function(tx,res) {
           ingreso = Number(res.rows.item(0).ingreso);
-          console.log("Ingrsos "+ingreso);
           tx.executeSql("SELECT SUM(monto_egresado) AS egresado from saldos_egreso WHERE strftime('%Y', fecha_egreso) = ?  ", [yy], function(tx,res) {
             egreso = Number(res.rows.item(0).egresado);
-            console.log("Egresos "+ egreso);
             document.getElementById('saldo_aca').innerHTML = "$ " + (ingreso - egreso);
           });
         });
@@ -66,26 +64,39 @@
       var ingreso;
       var egreso= 0;
       db.transaction(function(tx) {
-        console.log("***************************************************");
         tx.executeSql("SELECT SUM(monto_ingresado) AS ingreso from saldos_ingreso", [], function(tx,res) {
           ingreso = Number(res.rows.item(0).ingreso);
-          console.log("Ingrsos "+ingreso);
           tx.executeSql("SELECT SUM(monto_egresado) AS egresado from saldos_egreso", [], function(tx,res) {
             egreso = Number(res.rows.item(0).egresado);
-            console.log("Egresos "+ egreso);
             document.getElementById('patrimonio').innerHTML = "$ " + (ingreso - egreso);
           });
         });
       });
     }
 
-/*db.transaction(function (tx) {
-  tx.executeSql("select * from test_table;", [], function (tx,res) {
-  saldo_m.innerHTML = res.rows.item(0).saldo_m;
-  saldo_aca.innerHTML = res.rows.item(0).saldo_aca;
-  patrimonio.innerHTML =  res.rows.item(0).patrimonio;
-  alert(res);
-},function (error) {
-  alert("Error al realizar la petcicion")
-});
-});*/
+    function nivel_riqueza() {
+      var patrimonio = 0;
+      var promedio = 0;
+      var nivel_riqueza = 0;
+      db.transaction(function(tx) {
+        console.log("****************************************************");
+        tx.executeSql("SELECT SUM(monto_ingresado) AS ingreso from saldos_ingreso", [], function(tx,res) {
+          ingreso = Number(res.rows.item(0).ingreso);
+          tx.executeSql("SELECT SUM(monto_egresado) AS egresado from saldos_egreso", [], function(tx,res) {
+            egreso = Number(res.rows.item(0).egresado);
+            patrimonio = ingreso - egreso;
+            tx.executeSql("SELECT avg(monto_egresado) AS promedio from saldos_egreso",[],function (tx,res) {
+              promedio = Number(res.rows.item(0).promedio);
+              if (promedio != 0) {
+                nr = patrimonio/promedio;
+                document.getElementById('nivel_riqueza').innerHTML = nr.toFixed(2);
+              }else {
+                nr = "";
+                document.getElementById('nivel_riqueza').innerHTML = nr;
+              }
+              console.log(nr);
+            })
+          });
+        });
+      });
+    }
