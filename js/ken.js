2e4545547d3ab33e534ddac9cb6b3fc44d119cb6
juanortiz10 <juanjo.jjo94@gmail.com>
2015-09-25 15:56:45 -0500

    Avance Final

--- js/ken.js
+++ js/ken.js
@@ -1,63 +1,69 @@
- document.addEventListener("deviceready", onDeviceReady, false);
-    var db = window.openDatabase("gastos", "1.0", "local database", 200000); //will create database Dummy_DB or open it
- 
-    //function will be called when device ready
-    function onDeviceReady(){
-        db.transaction(populateDB, errorCB, successCB);
-    }
- 
-    //create table and insert some record
-    function populateDB(tx) {
-        tx.executeSql('Create Table IF NOT EXISTS categorias_ingreso(id_categoria_ingreso integer primary key, nombre_categoria_ingreso text )');
-        tx.executeSql('Create Table IF NOT EXISTS categorias_egreso(id_categoria_egreso integer primary key, nombre_categoria_egreso text )');
-        tx.executeSql('Create Table IF NOT EXISTS subcategorias_egreso(id_subcategoria_egreso integer primary key, nombre_subcategoria_egreso, id_categoria_egreso)');
-        tx.executeSql('Create Table IF NOT EXISTS saldos_ingreso(id_saldo_ingreso integer primary key, fecha_ingreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_ingresado real, id_categoria_ingreso integer)');
-        tx.executeSql('Create Table IF NOT EXISTS saldos_egreso(id_saldo_egreso integer primary key, fecha_egreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_egresado real, id_subcategoria_egreso integer)');
-        tx.executeSql('Create Table IF NOT EXISTS cta(id_cuenta_in integer primary key, nombre text, saldo real)');
-    }
- 
-    /*//function will be called when an error occurred
-    function errorCB(err) {
-        console.log("Error processing SQL: "+err.code);
-    }
- 
-    //function will be called when process succeed
-    function successCB() {
-        console.log("success!");
-        db.transaction(queryDB,errorCB);
-    }
- 
-    function queryDB(tx){
-        tx.executeSql('SELECT id_categoria_ingreso,nombre_categoria_ingreso FROM categorias_ingreso',[],querySuccess,errorCB);
-    }
- 
-    function querySuccess(tx,result){
-         $.each(result.rows,function(index){
-           var row = result.rows.item(index);
-          $('#main_table').append('<tr><td class="row"><a href="../views/three.html" onclick="sendId('+row['id_categoria_ingreso']+')">'+row['nombre_categoria_ingreso']+'</a></td></tr>');
-         });
-    }*/
-    
-    
-    
+document.addEventListener("deviceready", onDeviceReady, false);
+   var db = window.openDatabase("gastos", "1.0", "local database", 200000); //will create database Dummy_DB or open it
+
+   //function will be called when device ready
+   function onDeviceReady(){
+       db.transaction(populateDB, errorCB, successCB);
+   }
+
+   function populateDB(tx) {
+       tx.executeSql('Create Table IF NOT EXISTS categorias_ingreso(id_categoria_ingreso integer primary key, nombre_categoria_ingreso text )');
+       tx.executeSql('Create Table IF NOT EXISTS categorias_egreso(id_categoria_egreso integer primary key, nombre_categoria_egreso text )');
+       tx.executeSql('Create Table IF NOT EXISTS subcategorias_egreso(id_subcategoria_egreso integer primary key, nombre_subcategoria_egreso, id_categoria_egreso)');
+       tx.executeSql('Create Table IF NOT EXISTS saldos_ingreso(id_saldo_ingreso integer primary key, fecha_ingreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_ingresado real, id_categoria_ingreso integer)');
+       tx.executeSql('Create Table IF NOT EXISTS saldos_egreso(id_saldo_egreso integer primary key, fecha_egreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, monto_egresado real, id_subcategoria_egreso integer)');
+       tx.executeSql('Create Table IF NOT EXISTS cta(id_cuenta_in integer primary key, nombre text, saldo real)');
+   }
+
+   function errorCB(err) {
+       console.log("Error processing SQL: "+err.message);
+   }
+
+       //function will be called when process succeed
+   function successCB() {
+       console.log("correct");
+   }
+   function redirect(){
+        window.location.replace("../views/four.html");
+   }
+
+function agregarCuenta(){
+  var name=document.getElementById('nombre_cuenta').value;
+  db.transaction(function(tx){
+    tx.executeSql('INSERT INTO cta (nombre,saldo) VALUES (?,?)',[name,0],redirect);
+  });
+}
+
+function fillSelect(){
+  db.transaction(function(tx){
+    tx.executeSql('SELECT * FROM cta',[],function(tx,result){
+      for(var i=0; i<result.rows.length; i++){
+            var row = result.rows.item(i);
+            $('#select_cta').append('<option value="'+row['nombre']+'">'+row['nombre']+'</option>');
+      }
+    });
+  });
+}
+
+/*
   function checarCuenta(){
-	
+
 		db.transaction(function(tx){
 		tx.executeSql('SELECT COUNT(id_cuenta_in) AS cantidad FROM cta',function(tx,res){
 			var chequeo = res.rows.item(0).cantidad
-			
+
 			if(chequeo==0){
-    		
+
     		window.location=("../views/four-first.html");
     	}
     	else{
-    		
+
     		window.location=("../index.html");
     	}
 	});
 	});
 	}
-    
+
 
 
 function insertarNuevoSaldo(id) {
@@ -66,11 +72,11 @@ function insertarNuevoSaldo(id) {
     tx.executeSql('INSERT INTO cta(id_cuenta_in,saldo) VALUES (?,?)',[], successCB, errorCB);
   });
 }
-    
-    
-    
-    
-    
+
+
+
+
+
 
 
 
